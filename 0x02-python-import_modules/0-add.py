f7fefaf28d90810f5265e4f64699c67e2ac6bad4
JohnsonGith <johnsonwebsax@gmail.com>
2022-09-23 02:24:01 +0000

    files updated

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
+
+#break down
+#add(a, b) the function is inseerted in the print format
+#print("{} + {} + {}".format(a, b))
+
