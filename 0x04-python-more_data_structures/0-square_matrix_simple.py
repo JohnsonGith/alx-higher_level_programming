68e2c01a47bbcb8b715e2cdda10aaa71a0cc6eb3
JohnsonGith <johnsonwebsax@gmail.com>
2022-09-30 23:25:51 +0000

    files updated

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def squre_matrix_simple(matrix=[]):
+def square_matrix_simple(matrix=[]):
     if matrix is None:
         return None
-    return[[x **2 for x in row] for row in matrix]
+    return [[x ** 2 for x in row] for row in matrix]
