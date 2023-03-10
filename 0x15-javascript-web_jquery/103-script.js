38111a904189d244621a60784dc32754a9f5213d
JohnsonGith <johnsonwebsax@gmail.com>
2023-03-10 05:37:02 +0000

    file updated

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,17 +1,11 @@
-$('document').ready(function () {
-  $('INPUT#btn_translate').click(translate);
-  $('INPUT#language_code').focus(function () {
-    $(this).keydown(function (e) {
-      if (e.keyCode === 13) {
-        translate();
-      }
-    });
-  });
+$(document).ready(function () {
+	$("INPUT#btn_translate").click(function () {
+		const language_code = $("INPUT#language_code").val();
+		$.getJSON(
+			`https://fourtonfish.com/hellosalut/hello/?lang=${language_code}`,
+			function (data) {
+				$("#hello").text(data.hello);
+			}
+		);
+	});
 });
-
-function translate () {
-  const url = 'https://www.fourtonfish.com/hellosalut/?';
-  $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-    $('DIV#hello').html(data.hello);
-  });
-}
